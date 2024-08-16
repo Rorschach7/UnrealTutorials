29577a9ff1090d1e7f599e6807dded2f9f08aaf8
Rorschach7 <kevlicht@gmail.com>
2024-08-16 15:23:40 +0200

    added plugin tutorial content

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # UnrealTutorials
 A repository to serve as a collection for Unreal Engine tutorials.
+
+For tutorials check [here](https://www.lightcastle.dev/blog)
--- Source/Tutorials/Tutorials.Build.cs
+++ Source/Tutorials/Tutorials.Build.cs
@@ -10,7 +10,7 @@ public class Tutorials : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] { "MyPlugin" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
